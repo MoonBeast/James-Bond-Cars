1e020a43b2bf6c39c578c70bc188862d3abd5e72
Gorik <gorik_mcse@hotmail.com>
2019-05-27 15:17:30 -0400

    Start tracking JB_cars site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Animate smooth scroll
+$('#gallery').on('click', function () {
+    const images = $('#images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 800);
+});
